bbef8c9a7bf38241c7467232618751fd974a7c1d
Stephen Aguilar <stephenonline25@gmail.com>
2023-06-01 23:28:09 +0800

    Final Version

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
-#from .routes.firstlane import Firstlane
 
 app = Flask(__name__)
 app.secret_key = "981254678"
@@ -42,7 +41,14 @@ def success():
         if not lib:
             return "invalid username or password"
 
-        return render_template("libSuccess.html", logBool=logBool, lib=num)
+        count_books = countBooks()
+        available_books = countAvail()
+        pending_books = countPending()
+        unavailable_books = countNotAvail()
+        unpending_books = countNotPending()
+
+
+        return render_template("libSuccess.html", logBool=logBool, lib=num, available_books=available_books, pending_books=pending_books, count_books=count_books, unavailable_books=unavailable_books, unpending_books=unpending_books)
     
     elif logType == "stud":
         cur.execute("SELECT id FROM USERS WHERE username = %s and pass = %s", (username, password))
@@ -58,14 +64,19 @@ def success():
         if not user:
             return "invalid username or password"
 
-        return render_template("success.html", logBool=logBool, user=num)
+        count_books = countBooks()
+        available_books = countAvail()
+        pending_books = countPending()
+        unavailable_books = countNotAvail()
+        unpending_books = countNotPending()
+
+        return render_template("success.html", logBool=logBool, user=num, available_books=available_books, pending_books=pending_books, count_books=count_books, unavailable_books=unavailable_books, unpending_books=unpending_books)
 
 #register page
 @app.route("/register")
 def register():
     return render_template("register.html")
 
-#executes when all of the fields registered
 @app.route("/registering", methods=["POST"])
 def registering():
     name, bdate, username, password, password2 = request.form.get("fNameR").strip(), request.form.get("birthdateR"), request.form.get("usernameR").strip(), request.form.get("passR").strip(), request.form.get("passR2").strip()
@@ -93,7 +104,6 @@ def registering():
     cur.close()
     return redirect(url_for("index"))
 
-#loginpage actions
 @app.route("/actions", methods=["POST"])
 def actions():
     if "libraryBtn" in request.form:
@@ -127,7 +137,6 @@ def showBooks():
 @app.route("/searching", methods=["POST"])
 def searching():
     query, search = request.form.get("query"), request.form.get("search")
-    print(query)
     if len(query) == 0 or search is None:
         return "Invalid Parameters"
     cur = db.connection.cursor()
@@ -141,6 +150,54 @@ def searching():
     cur.close()
     return render_template("searched.html", books=books)
     
+@app.route("/searching2", methods=["POST"])
+def searching2():
+    query, search = request.form.get("query"), request.form.get("search")
+    if len(query) == 0 or search is None:
+        return "Invalid Parameters"
+    cur = db.connection.cursor()
+    if search == "Sisbn":
+        cur.execute("SELECT * FROM books_1 WHERE isbn LIKE %s", (query,))
+        
+    elif search == "Sbooks": 
+        cur.execute("SELECT * FROM books_1 WHERE title LIKE %s", (query,))
+    
+    books = cur.fetchall()
+    for row in books:
+        is_avail_value = int(row[6])
+        is_pending_value = int(row[7])
+    if(is_avail_value == 1):
+        is_avail = "Yes"
+    else:
+        is_avail = "No"
+    if(is_pending_value == 1):
+        is_pending = "Yes"
+    else:
+        is_pending = "No"
+    cur.close()
+    return render_template("libSearched.html", books=books, is_avail=is_avail, is_pending=is_pending)
+
+@app.route("/borrow", methods=["POST"])
+def borrow():
+    my_variable = request.form.get("isbn")
+    print(my_variable)
+    cur = db.connection.cursor()
+    cur.execute("SELECT is_avail, is_pending FROM books_1 WHERE isbn = %s", (my_variable,))
+    row = cur.fetchone()
+    is_avail = int(row[0])
+    is_pending = int(row[1])
+    print(is_pending)
+    if is_pending == 1:
+        return 'book is currenly at pending request not availabe <button type="button" onclick="window.history.go(-1); return false;">Back</button>'    
+    if is_avail == 0:
+        return 'book is currently is not available it is borrowed by someone <button type="button" onclick="window.history.go(-1); return false;">Back</button>'
+    
+    cur.execute("UPDATE books_1 SET is_pending = %s WHERE isbn = %s", ("1", my_variable))
+    db.connection.commit()
+    cur.close()
+    return 'success <button type="button" onclick="window.history.go(-1); return false;">Back</button>'
+    
+
 @app.route("/controls", methods=["POST"])
 def controls():
     if "libraryBtn" in request.form:
@@ -159,11 +216,32 @@ def controlsLib():
         elif "showBtn" in request.form:
             return redirect(url_for("libShowBooks"))
         elif "searchBtn" in request.form:
-            return render_template("searchbooks.html")
+            return render_template("libSearchbooks.html")
         elif "delBtn" in request.form:
             return render_template("delBooks.html")
+        elif "pendingBtn" in request.form:
+            return redirect(url_for("approval"))
+        elif "borrowedBtn" in request.form:
+            return redirect(url_for("borrowed"))
     else:
         return "method not allowed"
+
+@app.route("/borrowed", methods=["GET"])
+def borrowed():
+    cur = db.connection.cursor()
+    cur.execute("SELECT * FROM books_1 WHERE is_avail = 0")
+    data = cur.fetchall()
+    cur.close()
+    return render_template("libBorrowed.html", books=data)
+
+@app.route("/returned", methods=["POST"])
+def returned():
+    id = request.form.get("id")
+    cur = db.connection.cursor()
+    cur.execute("UPDATE books_1 SET is_avail = 1, is_pending = 0 WHERE id = %s", (id,))
+    db.connection.commit()
+    cur.close()
+    return redirect(url_for("borrowed"))
 
 @app.route("/adding", methods=["POST"])
 def adding():
@@ -173,6 +251,31 @@ def adding():
     db.connection.commit()
     cur.close()
     return redirect(url_for("libShowBooks"))
+
+@app.route("/approval", methods=["GET"])
+def approval():
+    cur = db.connection.cursor()
+    cur.execute("SELECT * FROM books_1 WHERE is_pending = 1")
+    data = cur.fetchall()
+    cur.close()
+    return render_template("libPending.html", books=data)
+
+@app.route("/approved", methods=["POST"])
+def approved():
+    if "approve" in request.form: 
+        id = request.form.get("id")
+        cur = db.connection.cursor()
+        cur.execute("UPDATE books_1 SET is_avail = 0, is_pending = 0 WHERE id = %s", (id,))
+        db.connection.commit()
+        cur.close()
+        return redirect(url_for("approval"))
+    elif "disapprove" in request.form:
+        id = request.form.get("id")
+        cur = db.connection.cursor()
+        cur.execute("UPDATE books_1 SET is_avail = 1, is_pending = 0 WHERE id = %s", (id,))
+        db.connection.commit()
+        cur.close()
+        return redirect(url_for("approval"))
 
 @app.route("/libShowBooks", methods=["GET"])
 def libShowBooks():
@@ -266,6 +369,41 @@ def checkUsername2(username):
                 return True
     cur.close()
     return False
+
+def countBooks():
+    cur = db.connection.cursor()
+    cur.execute("SELECT count(*) FROM books_1")
+    count = cur.fetchone()[0]
+    cur.close()
+    return count
+
+def countAvail():
+    cur = db.connection.cursor()
+    cur.execute("SELECT count(*) FROM books_1 WHERE is_avail = 1")
+    count = cur.fetchone()[0]
+    cur.close()
+    return count
+
+def countPending():
+    cur = db.connection.cursor()
+    cur.execute("SELECT count(*) FROM books_1 WHERE is_pending = 1")
+    count = cur.fetchone()[0]
+    cur.close()
+    return count
+
+def countNotAvail():
+    cur = db.connection.cursor()
+    cur.execute("SELECT count(*) FROM books_1 WHERE is_avail = 0")
+    count = cur.fetchone()[0]
+    cur.close()
+    return count
+
+def countNotPending():
+    cur = db.connection.cursor()
+    cur.execute("SELECT count(*) FROM books_1 WHERE is_pending = 0")
+    count = cur.fetchone()[0]
+    cur.close()
+    return count
 
 if __name__ == "__main__":
     app.run(debug=True)
